227dcf4ee2afd433ca879e91191aae1ea06a2548
hide06m <hide06m@gmail.com>
2026-08-25 12:16:03 +0900

    Section 14-4 Codexに保存の処理とメッセージ表示を作らせる

--- src/main/java/com/example/todoapp/HomeController.java
+++ src/main/java/com/example/todoapp/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,7 +21,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("title", "Todoアプリ");
+        model.addAttribute("title", "Todo\u30A2\u30D7\u30EA");
         return "index";
     }
 
@@ -52,7 +53,40 @@ public class HomeController {
     @PostMapping("/todos")
     public String create(@ModelAttribute Todo todo, RedirectAttributes redirectAttributes) {
         todoMapper.insert(todo);
-        redirectAttributes.addFlashAttribute("message", "登録しました");
+        redirectAttributes.addFlashAttribute("message", "\u767B\u9332\u3057\u307E\u3057\u305F");
+        return "redirect:/todos";
+    }
+
+    @GetMapping("/todos/{id}/edit")
+    public String edit(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
+        Todo todo = todoMapper.findById(id);
+        if (todo == null) {
+            redirectAttributes.addFlashAttribute("message", "\u898B\u3064\u304B\u308A\u307E\u305B\u3093\u3067\u3057\u305F");
+            return "redirect:/todos";
+        }
+        model.addAttribute("todo", todo);
+        return "edit";
+    }
+
+    @PostMapping("/todos/{id}/confirm")
+    public String editConfirm(@PathVariable Long id, @ModelAttribute Todo todo, Model model) {
+        todo.setId(id);
+        model.addAttribute("todo", todo);
+        return "edit-confirm";
+    }
+
+    @PostMapping("/todos/{id}/edit")
+    public String backToEdit(@PathVariable Long id, @ModelAttribute Todo todo, Model model) {
+        todo.setId(id);
+        model.addAttribute("todo", todo);
+        return "edit";
+    }
+
+    @PostMapping("/todos/{id}")
+    public String update(@PathVariable Long id, @ModelAttribute Todo todo, RedirectAttributes redirectAttributes) {
+        todo.setId(id);
+        todoMapper.update(todo);
+        redirectAttributes.addFlashAttribute("message", "保存しました");
         return "redirect:/todos";
     }
 }
